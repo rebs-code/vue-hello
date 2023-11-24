934d60ded358fa352d181303066afc478d082ed2
rebs-code <jack.rebussi@gmail.com>
2023-11-24 17:11:41 +0100

    feat: bonus 1 img with v-bind

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        imageSrc: "https://images.unsplash.com/photo-1682686581427-7c80ab60e3f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       }
     }
   }).mount('#app')
